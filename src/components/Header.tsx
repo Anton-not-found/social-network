0a64ccee3ad4f59d7719cb428da032d0fd1f08e6
Anton <shurmanov_anton@rambler.ru>
2023-02-04 13:20:58 +0300

    Added new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={s.header}>
+            <div className={s.headerLogo}>
+                <img
+                    src={'https://avatars.mds.yandex.net/i?id=6e33115c874306a061b58854df85a2b8-5518476-images-thumbs&n=13'} alt={'logo'}>
+                </img>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import s from './Navbar.module.css';
+const Navbar = () => {
+    return (
+        <div className={s.navbar}>
+            <div className={s.navbarItem}>
+                Profile
+            </div>
+            <div className={s.navbarItem}>
+                Messages
+            </div>
+            <div className={s.navbarItem}>
+                News
+            </div>
+            <div className={s.navbarItem}>
+                Music
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
