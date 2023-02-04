e8ab233b856f6b4336ea00d7593922c1ea86f37d
Anton <shurmanov_anton@rambler.ru>
2023-02-04 13:28:19 +0300

    Changed navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 News
             </div>
             <div className={s.navbarItem}>
-                Music
+                Musics
             </div>
         </div>
     );
